a5d89674b99f09f82c0f64575033e9dc9a2c0598
syeero7 <EmalHasitha@proton.me>
2024-12-13 19:09:58 +0530

    fix: add width and height attributes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ id, name, source, onClick }) {
   return (
     <figure data-id={id} onClick={onClick} className="card">
-      <img src={source} alt={name} />
+      <img src={source} alt={name} width="96" height="96" />
       <figcaption>{name}</figcaption>
     </figure>
   );
