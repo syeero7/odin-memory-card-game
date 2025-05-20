e2dafd8abe2483369889e823f869f9656bb0e128
Emal <EmalHasitha@proton.me>
2025-05-20 19:30:59 +0530

    refactor: relocate helper functions within Grid.jsx

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import { getPokemonData } from "../utils/pokemonData.js";
-import { shuffle } from "../utils/functions.js";
 import Card from "./Card.jsx";
 
 export default function Grid({ score, setScore }) {
@@ -44,3 +42,55 @@ export default function Grid({ score, setScore }) {
     </article>
   );
 }
+
+const getPokemonData = async () => {
+  const POKEMON_LIMIT = 1000;
+  const REQUIRED_POKEMON_COUNT = 20;
+  const numbers = getRandomNumbers(POKEMON_LIMIT, REQUIRED_POKEMON_COUNT);
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const pokemonData = [];
+
+  try {
+    for await (const response of numbers.map((num) => fetch(url + num))) {
+      if (!response.ok) throw new Error(`status : ${response.status}`);
+
+      const data = await response.json();
+      pokemonData.push({
+        id: data.id,
+        name: data.name,
+        sprite: data.sprites.front_default,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  return pokemonData;
+};
+
+const getRandomNumbers = (maxLimit, maxLength) => {
+  const numbers = {};
+  const randomNumbers = [];
+
+  while (randomNumbers.length !== maxLength) {
+    const randomNumber = Math.floor(Math.random() * maxLimit) + 1;
+
+    if (!numbers[randomNumber]) {
+      numbers[randomNumber] = 1;
+      randomNumbers.push(randomNumber);
+    }
+  }
+
+  return randomNumbers;
+};
+
+const shuffle = (array) => {
+  const newArray = [...array];
+
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+
+  return newArray;
+};
